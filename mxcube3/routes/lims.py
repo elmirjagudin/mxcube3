23b571f510c7e7d6dd0fcd8e0bf6142dd96925dd
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2018-12-20 08:41:40 +0100

    fix indentation

--- mxcube3/routes/lims.py
+++ mxcube3/routes/lims.py
@@ -150,10 +150,10 @@ def get_results():
 
         elif isinstance(model, qmo.Characterisation) or isinstance(model, qmo.Workflow):
             if result_file_test('characterisation-results.js'):
-		try:
+                try:
                     url_list =  data["limsResultData"]["workflow_result_url_list"]
-		except:
-		    url_list = None
+                except:
+                    url_list = None
                 if url_list:
                     r = jsonify({"result": run_get_result_script(join(mxcube.template_folder, 'characterisation-results.js'), url_list[0])})
                 else:
