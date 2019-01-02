e2522e42f63d8ad48ef7b67badc0db1c8fbc5193
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2019-01-02 11:24:54 +0100

    biomax custom gui

--- config.gui.prod.js
+++ config.gui.prod.js
@@ -1,5 +1,23 @@
 module.exports = {
-  phaseControl: false,
-  apertureControl: true,
-  focusControlOnCanvas: true
+	phaseControl: true,
+	focusControlOnCanvas: true,
+	apertureControl: true,
+	extendedCharacterisationParams: true,
+	helpLinks:[
+		{url: 'https://www.maxiv.lu.se/', name: 'MAX IV'},
+		{url: 'https://www.maxiv.lu.se/accelerators-beamlines/beamlines/biomax/', name: 'BioMAX'},
+		{url: 'https://wiki.maxiv.lu.se/index.php/Main_Page', name: 'MAX IV Wiki pages'}
+	],
+	beamlineCameras: [{
+		url: "http://172.16.119.11/mjpg/video.mjpg?streamprofile=Mobile",
+		width:"320px",
+		height:"180px",
+		location:"Experimental Hutch"
+		},
+		{
+		url: "http://172.16.119.9/mjpg/video.mjpg?streamprofile=Mobile",
+		width:"320px",
+		height:"180px",
+		location:"Diffractometer"
+	}]
 }
