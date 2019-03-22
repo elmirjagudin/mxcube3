9fa8c53df98853941e77a37d147955ac549f1b44
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2019-03-22 10:58:58 +0100

    Unselect on initial state
    Otherwise the shapes appear as selected in the canvas but the internal selecteShapes entry in the state
    is clear, leading to confusing situation.

--- mxcube3/ui/actions/general.js
+++ mxcube3/ui/actions/general.js
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-fetch';
+import { unselectShapes } from './sampleview';
 
 export function addUserMessage(record, target) {
   let duration = undefined;
@@ -229,6 +230,8 @@ export function getInitialState() {
 
     Promise.all(pchains).then(() => {
       dispatch(setInitialState(state));
+    }).then(() => {
+      dispatch(unselectShapes({ shapes: state.shapes }));
     });
   };
 }
