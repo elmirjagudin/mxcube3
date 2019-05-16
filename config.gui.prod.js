23ffd40644db6deb45a371a9f32c0f32e443adad
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2019-05-16 16:07:14 +0200

    new profiles for cameras

--- config.gui.prod.js
+++ config.gui.prod.js
@@ -9,13 +9,13 @@ module.exports = {
 		{url: 'https://wiki.maxiv.lu.se/index.php/Main_Page', name: 'MAX IV Wiki pages'}
 	],
 	beamlineCameras: [{
-		url: "http://172.16.119.11/mjpg/video.mjpg?streamprofile=Mobile",
+		url: "http://172.16.119.11/mjpg/video.mjpg?streamprofile=mxcube",
 		width:"320px",
 		height:"180px",
 		location:"Experimental Hutch"
 		},
 		{
-		url: "http://172.16.119.9/mjpg/video.mjpg?streamprofile=Mobile",
+		url: "http://172.16.119.9/mjpg/video.mjpg?streamprofile=mxcube",
 		width:"320px",
 		height:"180px",
 		location:"Diffractometer"
