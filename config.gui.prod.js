4e563460332c1a77436ce9c25e78e762cae36e8e
Jie Nan <jie.nan@maxiv.lu.se>
2020-04-01 16:33:02 +0200

    switch the experimental hutch camera

--- config.gui.prod.js
+++ config.gui.prod.js
@@ -9,7 +9,7 @@ module.exports = {
 		{url: 'https://wiki.maxiv.lu.se/index.php/Main_Page', name: 'MAX IV Wiki pages'}
 	],
 	beamlineCameras: [{
-		url: "http://172.16.119.11/mjpg/video.mjpg?streamprofile=mxcube",
+		url: "http://172.16.119.17/mjpg/video.mjpg?streamprofile=mxcube",
 		width:"320px",
 		height:"180px",
 		location:"Experimental Hutch"
